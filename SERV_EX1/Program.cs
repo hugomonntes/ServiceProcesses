cffef06807c9116d3b110a2a86014a46fddf7922
hmontes <hmontes.dev@gmail.com>
2025-10-06 13:06:17 +0200

    Agregar archivos de proyecto.

---
+++ SERV_EX1/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Drawing;
+
+namespace SERV_EX1
+{
+    internal class Program
+    {
+        //Recuerda que lo que se le pasa a continuación del comando de consola se
+        //recibe como array de strings en un parámetro del Main(string[] args).
+        //Puedes ver un ejemplo en el apartado Lista de Argumentos del Apéndice II del
+        //tema 3: Otros aspecto de C#.
+        //Los comandos serán los siguientes:
+
+        //1.Comando ls: se le puede pasar un directorio o una variable de entorno.
+        //Muestra el contenido de dicho directorio, tanto subdirectorios como archivos.
+        //Diferenciará por colores y en los archivos además indicará el tamaño de los
+        //mismos.
+
+
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ SERV_PROCESSES/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SERV_PROCESSES
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
