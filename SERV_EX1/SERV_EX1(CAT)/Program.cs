c7101a8cd704a85f9a2a32225d241bb9d90b9f1b
hmontes <hmontes.dev@gmail.com>
2025-10-08 10:38:45 +0200

    Update

---
+++ SERV_EX1/SERV_EX1(CAT)/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SERV_EX1_CAT_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
