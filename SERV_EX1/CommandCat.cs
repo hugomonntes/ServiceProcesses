cee0e8ae98c62d077777aef39a5fe62c66f21760
hugomonntes <huugomontees@gmail.com>
2025-10-10 22:17:25 +0200

    Update

--- SERV_EX1/CommandCat.cs
+++ SERV_EX1/CommandCat.cs
@@ -1,89 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SERV_EX1
+﻿namespace SERV_EX1
 {
-    internal class CommandCat
+    public class CommandCat
     {
-        //2. Comando cat: Se le pasa un archivo de texto.Muestrea el contenido del
-        //mismo en la consola.Si justo tras el comando tiene el argumento -n con un
-        //número a continuación solo mostrará la cantidad de líneas indicadas por
-        //dicho número (en este caso evita leer el archivo entero si no es necesario).
-        //Ejemplos:
-        //cat myfile.txt
-        //cat -n5 c:\windows\win.ini
-        public static bool checkArgsModifierN(string arg, out int number)
-        {
-            number = 0;
-            if (arg.StartsWith("-n"))
-            {
-                string numStr = arg.Substring(2);
-                if (int.TryParse(numStr, out int numberFormated))
-                {
-                    number = numberFormated;
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        public static bool checkFileExists(string fileName)
-        {
-            return File.Exists(fileName);
-        }
-
-        public static void showFileContent(string fileName, int numLinesToShow)
-        {
-            if (checkFileExists(fileName))
-            {
-                using StreamReader reader = new(fileName);
-                int contador = 0;
-                string? linea;
-
-                while ((linea = reader.ReadLine()) != null && contador < numLinesToShow)
-                {
-                    Console.WriteLine(linea);
-                    contador++;
-                }
-
-                if (contador == 0)
-                {
-                    Console.WriteLine("El archivo está vacío");
-                }
-            }
-        }
-
-        public static void showFullFile(string fileName) // En caso de que no me pase el modificador -n
-        {
-            if (checkFileExists(fileName))
-            {
-                using StreamReader reader = new(fileName);
-                string? linea;
-                while ((linea = reader.ReadLine()) != null)
-                {
-                    Console.WriteLine(linea);
-                }
-            }
-            else
-            {
-                Console.WriteLine("El archivo no existe");
-            }
-
-        }
-
-        public static void commandCat(string[] args) // -n50[0] ruta[1]
+        public static void commandCat(string[] args)
         {
             if (args.Length > 0)
             {
-                if (checkArgsModifierN(args[0], out int numLineas))
+                if (GeneralCommands.checkArgsModifier(args[0], out int numLineas, "-n"))
                 {
-                    if (checkFileExists(args[1]))
+                    if (GeneralCommands.checkFileExists(args[1]))
                     {
-                        string fileName = args[1];
-                        showFileContent(fileName, numLineas);
+                        GeneralCommands.showFileContent(args[1], numLineas);
                     }
                     else
                     {
@@ -93,14 +20,13 @@ namespace SERV_EX1
                 else
                 {
                     string fileName = args[0];
-                    showFullFile(fileName);
+                    GeneralCommands.showFullFile(fileName);
                 }
             }
             else
             {
-                Console.WriteLine("Usa así cat -nX ruta");
+                Console.WriteLine("Usa así: cat [-nX] ruta");
             }
-
         }
     }
 }
---
+++ SERV_EX1/CommandNewFile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SERV_EX1
+{
+    public class CommandNewFile
+    {
+        //3. Comando newfile: Crea un archivo con el texto que haya a continuación.
+        //Si justo tras el comando se pone -a, añade el texto en vez de sobreescribirlo.
+        //Ejemplos:
+        //newfile myfile.txt “This text goes in the myfile.”
+        //newfile -a miarchivo.txt “And this one is added.”
+        
+        public static void crearCommandNewFile(string[] args)
+        {
+            if (GeneralCommands.checkArgsModifier(args[0], out int number, "-a"))
+            {
+                GeneralCommands.checkFileExists(args[1]);
+            }
+        }
+    }
+}
