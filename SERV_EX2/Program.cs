884df3bea97125e09c8dbd805f3a80d139784954
hugomonntes <huugomontees@gmail.com>
2025-10-11 15:11:56 +0200

    Update

---
+++ SERV_EX2/Program.cs
@@ -0,0 +1,13 @@
+﻿using DI_EX7;
+using System;
+
+namespace SERV_EX2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Astro> aa = DI_EX7.Program.coleccionAstros;
+        }
+    }
+}
