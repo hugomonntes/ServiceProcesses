63960ca3d9a370024ea370f21cc7de5b98069539
hugomonntes <huugomontees@gmail.com>
2025-10-20 21:53:12 +0200

    Add requestOperationType()

--- SERV_EX4/Program.cs
+++ SERV_EX4/Program.cs
@@ -5,25 +5,6 @@ namespace SERV_EX4
 {
     internal class Program
     {   
-        public static bool isAprove(int[] califications, ref int[] aproveCalifications)
-        {
-            int j = 0;
-            for (int i = 0; i < califications.Length; i++)
-            {
-                if (califications[i] >= 5)
-                {
-                    aproveCalifications[j] = califications[i];
-                    j++;
-                }
-            }
-            return aproveCalifications.Length > 0;
-        }
-
-        public static void showAproveCalifications(int[] califications)
-        {
-
-        }
-
         static void Main(string[] args)
         {
             //Crea estos dos vectores en un Main:
@@ -32,10 +13,17 @@ namespace SERV_EX4
             //Usa métodos de la clase Array junto con lambdas para obtener directamente
             //estos elementos del vector notas:
             //• Saber de si hay algún aprobado(Si existe o no) en notas.
+            Console.WriteLine(notas.Any(n => n >= 5));
             //• Mostrar los aprobados de notas.
+            int[] notasAprobadas = notas.Where(n => n >= 5).ToArray();
+            Array.ForEach(notasAprobadas, n => Console.WriteLine(n));
             //• Indicar la posición en el array del último aprobado
+            int ultimaPosicionAprobado = Array.FindLastIndex(notas, n => n >= 5);
+            Console.WriteLine(ultimaPosicionAprobado);
             //• Mostrar la nota del último aprobado.
+            Console.WriteLine(notas.Last(n => n >= 5));
             //• Cuanto tienen nota par.
+            Console.WriteLine(notas.);
             //Y ahora haz lo siguiente con el vector palabras:
             //• Cual es la primera palabra de más de 3 caracteres.
             //• Mostrar todas las palabras en mayúsculas.
