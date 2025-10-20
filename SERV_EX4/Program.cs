b6a27c40776db4a6fb3995e22190d846e86f0243
hugomonntes <huugomontees@gmail.com>
2025-10-20 22:30:59 +0200

    Add requestOperationType()

--- SERV_EX4/Program.cs
+++ SERV_EX4/Program.cs
@@ -13,21 +13,23 @@ namespace SERV_EX4
             //Usa métodos de la clase Array junto con lambdas para obtener directamente
             //estos elementos del vector notas:
             //• Saber de si hay algún aprobado(Si existe o no) en notas.
-            Console.WriteLine(notas.Any(n => n >= 5));
+            Console.WriteLine($"Hay aprobados: {notas.Any(n => n >= 5)}");
             //• Mostrar los aprobados de notas.
             int[] notasAprobadas = notas.Where(n => n >= 5).ToArray();
-            Array.ForEach(notasAprobadas, n => Console.WriteLine(n));
+            Array.ForEach(notasAprobadas, n => Console.WriteLine($"Aprobados con un {n}"));
             //• Indicar la posición en el array del último aprobado
-            int ultimaPosicionAprobado = Array.FindLastIndex(notas, n => n >= 5);
-            Console.WriteLine(ultimaPosicionAprobado);
+            Console.WriteLine($"Ultimo aprobado en la posicion:  {Array.FindLastIndex(notas, n => n >= 5)}");
             //• Mostrar la nota del último aprobado.
-            Console.WriteLine(notas.Last(n => n >= 5));
+            Console.WriteLine($"Nota ultimo aprobado {notas.Last(n => n >= 5)}");
             //• Cuanto tienen nota par.
-            Console.WriteLine(notas.);
+            Console.WriteLine($"Cuantos tienen nota par {notas.Count(n => n % 2 == 0)}");
             //Y ahora haz lo siguiente con el vector palabras:
             //• Cual es la primera palabra de más de 3 caracteres.
+            Console.WriteLine($"La primera palabra de más de 3 caracteres es: {palabras.First(s => s.Length > 3)}");
             //• Mostrar todas las palabras en mayúsculas.
+            Array.ForEach(palabras, palabra => Console.WriteLine($"Palabra en mayus: {palabra.ToUpper()}"));
             //• Indica la posición de la primera palabra que empiece por E
+            Console.WriteLine($"La primera palabra que empieza por E esta en la posicion: {Array.IndexOf(palabras,palabras.First(palabra => palabra.StartsWith("E")))}");
 
         }
     }
