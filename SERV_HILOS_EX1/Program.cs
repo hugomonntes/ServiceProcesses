caba056f10a83ae121d6565c465c0b5cc78bb38a
hmontes <hmontes.dev@gmail.com>
2025-10-29 10:08:34 +0100

    Update

---
+++ SERV_HILOS_EX1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.Intrinsics.X86;
+using System.Threading;
+
+namespace SERV_HILOS_EX1
+{
+    internal class Program
+    {
+        //a) Crea dos hilos(Thread) compitiendo que traten de incrementar(el primer hilo) o
+        //decrementar(el segundo hilo) en 1 unidad una variable que comienza en 0.
+        //Funcionarán de forma continua hasta que a llegue a 500(primer hilo) o a -500
+        //(segundo hilo).Además se mostrará en pantalla la variable cada vez que cambie
+        //indicando quién la ha cambiado(thread 1 o thread 2 y cada uno en un color).
+        //Ambos hilos deben parar en cuanto uno consiga su objetivo. No uses
+        //setCursorPosition.
+        //El Main, una vez que lanza los hilos, se queda en espera hasta que ambos hilos
+        //finalizan, luego informa de cual ha ganado.
+        //b) Las funciones de hilos serán expresiones lambda(si quieres y los ves claro haz
+        //ya directamente este apartado).
+
+        public static void counter()
+        {
+            for (int i = 1; i <= 500000; i++)
+            {
+                Console.SetCursorPosition(1, 1);
+                Console.WriteLine(i);
+            }
+        }
+
+        public static void createThreads()
+        {
+            Thread thread1 = new Thread(counter);
+            thread1.Start();
+        }
+
+        static void Main(string[] args)
+        {
+            createThreads();
+        }
+    }
+}
