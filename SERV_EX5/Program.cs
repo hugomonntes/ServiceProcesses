4ed801b75cb5b5f2b54cde5d5a44af5126d95ae5
hugomonntes <huugomontees@gmail.com>
2025-11-03 12:45:39 +0100

    Add ex serv 5

---
+++ SERV_EX5/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace SERV_EX5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
+//a) Desarrolla una función para la realización de cualquier menú mediante
+//delegados. Dicha función se denominará MenuGenerator y tendrá dos
+//parámetros: El primero un vector de strings con los nombres de las opciones.
+//El segundo un vector de delegados (hazlos que devuelvan void y sin
+//parámetros y el nombre del delegado que sea MyDelegate) que serán las
+//funciones que correspondan a cada opción del menú.
+
