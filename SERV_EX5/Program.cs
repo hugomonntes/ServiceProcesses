c12f2f1191b6e8f022351754c778db7c0abbd02c
hugomonntes <huugomontees@gmail.com>
2025-11-03 16:17:32 +0100

    Add ex serv 5

--- SERV_EX5/Program.cs
+++ SERV_EX5/Program.cs
@@ -1,17 +1,57 @@
-﻿namespace SERV_EX5
+﻿using System.Reflection.Emit;
+
+namespace SERV_EX5
 {
     internal class Program
     {
         public delegate void MyDelegate();
 
-        public static void menuGenerator(String[] optionsName, MyDelegate[] delegates)
+        public static bool menuGenerator(String[] optionsName, MyDelegate[] functions)
         {
-            
+            int optionSelected;
+            if (optionsName.Length == functions.Length)
+            {
+                do
+                {
+                    for (int i = 0; i < optionsName.Length; i++)
+                    {
+                        Console.WriteLine($"{i + 1}.- {optionsName[i]}");
+                    }
+                    Console.WriteLine($"{optionsName.Length + 1} Salir");
+                    Console.WriteLine("Elige una opcion: ");
+                    string option = Console.ReadLine();
+                    if (int.TryParse(option, out optionSelected))
+                    {
+                        if (optionSelected >= 1 && optionSelected <= optionsName.Length)
+                        {
+                            functions[optionSelected - 1]();
+                        }
+                        else if (optionSelected == optionsName.Length + 1)
+                        {
+                            Console.WriteLine("Gracias por usar el programa");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Opcion no válida");
+                        }
+                    }
+                }
+                while (optionSelected != optionsName.Length + 1);
+            }
+            else
+            {
+                Console.WriteLine("Error el tamaño de las opciones es diferente al de las funciones");
+            }
+            return true;
         }
-
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            menuGenerator(new string[] { "Op1", "Op2", "Op3" }, 
+            new MyDelegate[] { 
+            () => Console.WriteLine("A"),
+            () => Console.WriteLine("B"),
+            () => Console.WriteLine("C")
+            });
         }
     }
 }
