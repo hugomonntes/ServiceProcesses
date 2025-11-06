1d8f0ca96a35c1155649ac27922747f224ea7316
hmontes <hmontes.dev@gmail.com>
2025-11-06 11:59:02 +0100

    Add: init horse race

--- SERV_HILOS_EX2/Program.cs
+++ SERV_HILOS_EX2/Program.cs
@@ -8,32 +8,35 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace SERV_HILOS_EX1
 {
-    internal class Program // TODO cambiar list por array
+    internal class Program
     {
         //Realiza en consola el juego de carreras de caballos con al menos 5 caballos(haz
         //un array de hilos) pero teniendo en cuenta que ahora cada caballo es un objeto de
         //tipo Thread.El usuario hace su apuesta y luego empieza la carrera de caballos de
         //forma que cada uno se mueve una distancia aleatoria y “duerme” un tiempo
         //aleatorio.Al empezar la carrera el Main (o la función inicial) se queda en espera.
+
         //Una vez que un caballo llega a la meta todos paran y el main continúa indicando el
         //caballo ganador y si el usuario ha ganado. Se permitirá la repetición del juego. No
         //uses expresiones lambda en este ejercicio.
+
         //Visualmente deben verse en varias líneas los caballos avanzar, cada uno en la suya.
         //Puede ser tan simple como un asterisco que avance desde el principio de la línea
         //hasta la meta.
+
         //Nota: De cara a realizar pruebas de este juego, se recomienda quitar la
         //aleatoriedad temporalmente para forzar a que varios caballos lleguen a un tiempo y
         //ver que solo uno es el que “cruza” la meta.
-        public static void initThreads(List<Thread> horsesThreads, int numThreads, int randomNumber)
+        public static void initThreads(Thread[] horsesThreads, int numThreads, int randomNumber)
         {
             int counter = 0;
             for (int i = 0; i < numThreads; i++)
             {
-                horsesThreads.Add(new Thread(() =>
+                horsesThreads[i] = new Thread(() =>
                 {
                     counter += randomNumber;
                     Console.WriteLine(counter);
-                }));
+                });
             }
         }
 
@@ -45,8 +48,16 @@ namespace SERV_HILOS_EX1
 
         static void Main(string[] args)
         {
-            List<Thread> horsesThreads = new List<Thread>();
-            initThreads(horsesThreads, 5, getRandomNumber(10));
+            Thread[] horsesThreads = new Thread[5];
+            int counter = 0;
+            for (int i = 0; i < horsesThreads.Length; i++)
+            {
+                horsesThreads[i] = new Thread(() =>
+                {
+                    counter += getRandomNumber(5);
+                    Console.WriteLine(counter);
+                });
+            }
         }
     }
 }
