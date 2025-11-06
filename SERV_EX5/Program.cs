82880534f051c86a780936ad11fc28f8b5760b46
hugomonntes <huugomontees@gmail.com>
2025-11-06 20:49:59 +0100

    fix(ex5): fix bugs on conditionals

--- SERV_EX5/Program.cs
+++ SERV_EX5/Program.cs
@@ -9,45 +9,62 @@ namespace SERV_EX5
         public static bool menuGenerator(String[] optionsName, MyDelegate[] functions)//DEvolver false si hay problema. Mejorar comprobaciones. Mensajes de error.
         {
             int optionSelected;
-            if (optionsName.Length == functions.Length)
+            if (optionsName == null || functions == null)
             {
-                do
+                Console.WriteLine("Error alguno de los arrays es nulo.");
+                return false;
+            }
+
+            if (optionsName.Contains(null) || functions.Contains(null))
+            {
+                Console.WriteLine("Error alguno de los componentes de los arrays es nulo.");
+                return false;
+            }
+
+            if (optionsName.Length != functions.Length)
+            {
+                Console.WriteLine("Error los arrays no tienen la misma longitud.");
+                return false;
+            }
+
+            do
+            {
+                for (int i = 0; i < optionsName.Length; i++)
                 {
-                    for (int i = 0; i < optionsName.Length; i++)
+                    Console.WriteLine($"{i + 1}.- {optionsName[i]}");
+                }
+                Console.WriteLine($"{optionsName.Length + 1}.- Salir");
+
+                Console.Write("Elige una opción: ");
+                string option = Console.ReadLine();
+
+                if (int.TryParse(option, out optionSelected))
+                {
+                    if (optionSelected >= 1 && optionSelected <= optionsName.Length)
                     {
-                        Console.WriteLine($"{i + 1}.- {optionsName[i]}");
+                        functions[optionSelected - 1]();
                     }
-                    Console.WriteLine($"{optionsName.Length + 1} Salir");
-                    Console.WriteLine("Elige una opcion: ");
-                    string option = Console.ReadLine();
-                    if (int.TryParse(option, out optionSelected))
+                    else if (optionSelected == optionsName.Length + 1)
                     {
-                        if (optionSelected >= 1 && optionSelected <= optionsName.Length)
-                        {
-                            functions[optionSelected - 1]();
-                        }
-                        else if (optionSelected == optionsName.Length + 1)
-                        {
-                            Console.WriteLine("Gracias por usar el programa");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Opcion no válida");
-                        }
+                        Console.WriteLine("Saliendo...");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Opción fuera de rango.");
                     }
                 }
-                while (optionSelected != optionsName.Length + 1);
+                else
+                {
+                    Console.WriteLine("Introduce un número.");
+                }
             }
-            else
-            {
-                Console.WriteLine("Error el tamaño de las opciones es diferente al de las funciones");
-            }
+            while (optionSelected != optionsName.Length + 1);
             return true;
         }
         static void Main(string[] args)
         {
-            menuGenerator(new string[] { "Op1", "Op2", "Op3", "op4" }, 
-            new MyDelegate[] { 
+            menuGenerator(new string[] { "Op1", "Op2", "Op3", "op4" },
+            new MyDelegate[] {
             () => Console.WriteLine("A"),
             () => Console.WriteLine("B"),
             () => Console.WriteLine("C"),
