0b4538763497cab5b8291e783b763f2638bd5575
hugomonntes <huugomontees@gmail.com>
2025-11-23 00:08:42 +0100

    Update

--- SERV_HILOS_EX4/Form1.Designer.cs
+++ SERV_HILOS_EX4/Form1.Designer.cs
@@ -28,27 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtUrl = new System.Windows.Forms.TextBox();
             this.btnBusqueda = new System.Windows.Forms.Button();
             this.btnPosicion = new System.Windows.Forms.Button();
             this.btnHttp = new System.Windows.Forms.Button();
+            this.txbChars = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // textBox1
+            // txtUrl
             // 
-            this.textBox1.Location = new System.Drawing.Point(151, 39);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(413, 20);
-            this.textBox1.TabIndex = 0;
+            this.txtUrl.Location = new System.Drawing.Point(151, 39);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(413, 20);
+            this.txtUrl.TabIndex = 0;
             // 
             // btnBusqueda
             // 
-            this.btnBusqueda.Location = new System.Drawing.Point(151, 65);
+            this.btnBusqueda.Location = new System.Drawing.Point(12, 130);
             this.btnBusqueda.Name = "btnBusqueda";
             this.btnBusqueda.Size = new System.Drawing.Size(75, 23);
             this.btnBusqueda.TabIndex = 1;
             this.btnBusqueda.Text = "Busqueda";
             this.btnBusqueda.UseVisualStyleBackColor = true;
+            this.btnBusqueda.Click += new System.EventHandler(this.btnBusqueda_Click);
             // 
             // btnPosicion
             // 
@@ -68,15 +70,23 @@
             this.btnHttp.Text = "HTTP";
             this.btnHttp.UseVisualStyleBackColor = true;
             // 
+            // txbChars
+            // 
+            this.txbChars.Location = new System.Drawing.Point(93, 133);
+            this.txbChars.Name = "txbChars";
+            this.txbChars.Size = new System.Drawing.Size(145, 20);
+            this.txbChars.TabIndex = 4;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txbChars);
             this.Controls.Add(this.btnHttp);
             this.Controls.Add(this.btnPosicion);
             this.Controls.Add(this.btnBusqueda);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtUrl);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -86,10 +96,11 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtUrl;
         private System.Windows.Forms.Button btnBusqueda;
         private System.Windows.Forms.Button btnPosicion;
         private System.Windows.Forms.Button btnHttp;
+        private System.Windows.Forms.TextBox txbChars;
     }
 }
 
--- SERV_HILOS_EX4/Form1.cs
+++ SERV_HILOS_EX4/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,9 +18,35 @@ namespace SERV_HILOS_EX4
             InitializeComponent();
         }
 
-        public string BuscaPalabra(string path, string wordToSearch)
+        public string BuscaPalabra(string path, string wordToSearch) // hugo
         {
-            return $"";
+            int contador = 0;
+            using (StreamReader sreader = new StreamReader(path))
+            {
+                string allText = sreader.ReadToEnd(); //hola a hola asd dasdafa asdfasdfasdf sadfas asdf asdfasdfa sdf asdfasdfa
+                for (int i = 0; i < allText.Length - wordToSearch.Length; i++)
+                {
+                    if (allText.Substring(i, wordToSearch.Length) == wordToSearch)
+                    {
+                        contador++;
+                    }
+                }
+            }
+            
+            return $"{Path.GetFileName(path)},{wordToSearch},{contador}";
+        }
+
+        private void btnBusqueda_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(txtUrl.Text); // Comprobar vacíos, nulls (Funcion comprobar campos de todos txb)
+            FileInfo[] archivos = directoryInfo.GetFiles();
+            foreach (FileInfo archivo in archivos)
+            {
+                if (archivo.Extension == ".txt")
+                {
+                    Task<string> tarea = Task.Run(() => BuscaPalabra(archivo.FullName, txbChars.Text));
+                }
+            }
         }
     }
 }
