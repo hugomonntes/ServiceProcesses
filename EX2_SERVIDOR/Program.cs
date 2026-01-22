e53fe51f2635a5b6a172791bc3d4394f09336f3e
hmontes <hmontes.dev@gmail.com>
2026-01-22 12:20:04 +0100

    feat(server): add methods init, stop, manager to server

--- EX2_SERVIDOR/Program.cs
+++ EX2_SERVIDOR/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Server server = new Server();
+            server.Init();
         }
     }
 }
--- EX2_SERVIDOR/Server.cs
+++ EX2_SERVIDOR/Server.cs
@@ -11,43 +11,81 @@ namespace EX2_SERVIDOR
     internal class Server
     {
         private List<Usuario> usuarios = new List<Usuario>();
-        private IPAddress ip;
         private int port;
+        private bool ServerIsRunning = false;
         public static Socket socket;
+        public string userName;
 
         public void Init()
         {
-            IPEndPoint endPoint = new IPEndPoint(ip, port);
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, port);
             using (socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
             {
-
-
+                socket.Bind(endPoint);
+                socket.Listen(500); // TODO cambiar numero escuchas
+                do
+                {
+                    Socket client = socket.Accept();
+                    Thread thread = new Thread(() => ClientManager(client));
+                }
+                while (ServerIsRunning);
             }
         }
 
-        public bool isFreePort(int port)
+        public int isFreePort(int port)
         {
-            IPEndPoint endPoint = new IPEndPoint(ip, port);
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, port);
             using (socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
             {
-                try
+                bool isFree = true;
+                do
                 {
-                    socket.Bind(endPoint);
-                    socket.Listen(1);
+                    try
+                    {
+                        socket.Bind(endPoint);
+                        socket.Listen(1);
+                    }
+                    catch (SocketException s) when (s.SocketErrorCode == SocketError.AddressAlreadyInUse) // TODO OJO utilizo propiedad socketError no ErrorCode
+                    {
+                        port++;
+                        isFree = false;
+                    }
+                    catch (SocketException)
+                    {
+                        port++;
+                        isFree = false;
+                    }
                 }
-                catch (SocketException s) when (s.SocketErrorCode == SocketError.AddressAlreadyInUse) // TODO OJO utilizo propiedad socketError no ErrorCode
-                {
-                    port++;
-                } catch (SocketException)
-                {
-                    port++;
-                }
+                while (!isFree);
+                return port;
             }
         }
 
         public void Stop()
         {
 
+        }
+
+        public void ClientManager(Socket socketClient)
+        {
+            using (socketClient)
+            {
+                IPEndPoint clientEndPoint = (IPEndPoint)socketClient.RemoteEndPoint;
+                using (NetworkStream network = new NetworkStream(socketClient))
+                using (StreamWriter sw = new StreamWriter(network, Console.OutputEncoding))
+                using (StreamReader sr = new StreamReader(network, Console.OutputEncoding))
+                {
+                    sw.AutoFlush = true;
+                    try
+                    {
+                        userName = sr.ReadLine();
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+                }
+            }
         }
     }
 }
