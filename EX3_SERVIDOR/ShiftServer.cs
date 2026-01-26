b7263de0cdbfe2c88c03cc0fc797af4047e49068
hmontes <hmontes.dev@gmail.com>
2026-01-26 13:15:42 +0100

    feat(ShiftServer): add methods and properties on class shiftServer

--- EX3_SERVIDOR/ShiftServer.cs
+++ EX3_SERVIDOR/ShiftServer.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -28,7 +30,7 @@ namespace EX3_SERVIDOR
 
         public int readPin(string path)
         {
-            int[] pin = new int[4]; 
+            string pin = "";
             try
             {
                 using (StreamReader sr = new StreamReader(path))
@@ -36,7 +38,7 @@ namespace EX3_SERVIDOR
                     string content = sr.ReadToEnd().Trim(); // OJO con excepcion en caso de null (probar)
                     for (int i = 0; i < 4; i++)
                     {
-                        pin[i] = content[i];
+                        pin += content[i];
                     }
                 }
             }
@@ -45,7 +47,58 @@ namespace EX3_SERVIDOR
                 Console.WriteLine("Error con el archivo");
                 return -1;
             }
-            return pin[0];
+            return int.Parse(pin) | -1;
+        }
+
+        public bool isFreePort(int port)
+        {
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Any, port);
+            using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                try
+                {
+                    socket.Bind(iPEndPoint);
+                    socket.Listen(1);
+                }
+                catch (SocketException)
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+
+        public int getFreePort(int initialPort)
+        {
+            IPEndPoint iP = new IPEndPoint(IPAddress.Any, initialPort);
+            using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                bool isFree = false;
+                do
+                {
+                    try
+                    {
+                        socket.Bind(iP);
+                        socket.Listen(1);
+                        isFree = true;
+                    }
+                    catch (SocketException)
+                    {
+                        initialPort++;
+                    }
+                }
+                while (!isFree && initialPort < IPEndPoint.MaxPort); // Comprobar lógica del bucle
+                return initialPort;
+            }
+        }
+
+        public void Init()
+        {
+            int port = 31416;
+            if (isFreePort(port))
+            {
+                port = getFreePort(1024);
+            }
         }
     }
 }
